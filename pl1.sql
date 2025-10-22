79660578298cb24b7f1f5039dc02e2595da3cb60
ruben <ruben.sobrinor@edu.uah.es>
2025-10-22 19:09:06 +0200

    creacion tablas intermedias

--- pl1.sql
+++ pl1.sql
@@ -1,4 +1,123 @@
 BEGIN;
+-- Creacion de tablas intermedias para la carga de datos
+CREATE TABLE Ciruitos_Int(
+    Id  TEXT ,
+    Referncia TEXT,
+    Nombre TEXT,
+    Localizacion TEXT,
+    Pais TEXT ,
+    Longitud TEXT,
+    Latitud TEXT,
+    Altura TEXT,
+    Url TEXT
+)
+
+CREATE TABLE Escuderias_Int(
+    Id TEXT,
+    Referncia TEXT,
+    Nombre TEXT,
+    Nacionalidad TEXT,
+    Url TEXT,
+)
+
+CREATE TABLE Pilotos_Int(
+    Id TEXT;
+    Referencia TEXT;
+    Numero TEXT;
+    Codigo TEXT;
+    Nombre TEXT;
+    Apellido TEXT;
+    Fecha_Nac TEXT;
+    Nacionalidad TEXT;
+    Url TEXT;
+)
+
+CREATE TABLE Tiempo_vuelta_int(
+    Id TEXT;
+    Id_piloto TEXT;
+    Vuelta TEXT;
+    Posicion TEXT;
+    Tiempo TEXT;
+    Mls TEXT;
+)
+
+CREATE TABLE Pit_stop_int(
+    Id_carrera TEXT;
+    Id_piloto TEXT;
+    Num_paradas TEXT;
+    Vueltas TEXT;
+    Tiempo TEXT;
+    Duracion TEXT;
+    Mls TEXT;
+)
+
+CREATE TABLE Clasificacion_int(
+    Id TEXT;
+    Id_carrera TEXT;
+    Id_piloto TEXT;    
+    Id_escuderia TEXT;
+    Num_piloto TEXT;
+    Posicion TEXT;
+    Q1 TEXT;
+    Q2 TEXT;
+    Q3 TEXT;
+)
+
+CREATE TABLE Carrera_Int(
+    Id TEXT;
+    Año TEXT;
+    Ronda TEXT;
+    Id_circuito TEXT;
+    Nombre TEXT;
+    Fecha TEXT;
+    Hora TEXT;
+    Url TEXT;
+    FP1_fecha TEXT;
+    FP1_hora TEXT;
+    FP2_fecha TEXT;
+    FP2_hora TEXT;
+    FP3_fecha TEXT;
+    FP3_hora TEXT;
+    Fecha_quali TEXT;
+    Hora_quali TEXT;
+    Fecha_sprint TEXT;
+    Hora_sprint TEXT;
+)
+
+CREATE TABLE Resultados_int(
+    Id TEXT;
+    Id_gp TEXT;
+    Id_piloto TEXT;
+    Id_escuderia TEXT;
+    Numero TEXT;
+    Pos_parrilla TEXT;
+    Posicion TEXT;
+    Posicion_texto TEXT;
+    Posicion_orden TEXT;
+    Puntos TEXT;
+    Vueltas TEXT;
+    Tiempo TEXT;
+    Tiempo_mls TEXT;
+    Vuelta_rapida TEXT;
+    Puesto_campeonato TEXT;
+    Tiempo_vuelta_rapida TEXT;
+    Velocidad_vuelta_rapida TEXT;
+    Id_estado TEXT;
+)
+
+CREATE TABLE Temporadas_int(
+    Año TEXT;
+    Url TEXT;
+)
+
+CREATE TABLE Estado_int(
+    Id TEXT;
+    Estado TEXT;
+)
+
+
+
+
 
 -- =============================
 -- 1. ESCUDERÍA
